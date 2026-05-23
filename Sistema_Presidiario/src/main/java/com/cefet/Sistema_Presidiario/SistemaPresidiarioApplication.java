537150fd2bda3363630bf58c6c5c44daaacab9ee
deividbss128-pixel <deividbss128@gmail.com>
2026-05-23 15:44:01 -0300

    Organização inicial
    crie os packages dentro do main, fiz o application.propieties e já adicionei as dependencias de segurança no pom.xml

---
+++ Sistema_Presidiario/src/main/java/com/cefet/Sistema_Presidiario/SistemaPresidiarioApplication.java
@@ -0,0 +1,13 @@
+package com.cefet.Sistema_Presidiario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPresidiarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPresidiarioApplication.class, args);
+	}
+
+}
---
+++ Sistema_Presidiario/src/test/java/com/cefet/Sistema_Presidiario/SistemaPresidiarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cefet.Sistema_Presidiario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPresidiarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
